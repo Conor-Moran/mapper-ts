6ef5ff31ebcbd8e097b8cd9caafd7905e52957d5
Conor Moran <conor.j.moran@gmail.com>
2023-06-18 13:57:05 +0100

    builder for composing fieldmap

--- ts/app.ts
+++ ts/app.ts
@@ -1,5 +1,8 @@
 export function main() {
-    map(source, fieldMap);
+    //console.log(map(source, fieldMap));
+    //p(data, trio);
+    //console.log(trio);
+    console.log(doMap());
 };
 
 const source = {
@@ -52,29 +55,143 @@ function inflate(obj: { [x: string]: any; }, prefix = '') {
     return inflated;
 }
 
-class Person {
-    constructor(
-        private firstName: string,
-        private lastLast: string,
-        private nums: number
-      ) {}
-}
-
 const fieldMap: [string, string][] = [
     ['oper.givenName', 'debtorAcc.agent.firstName'],
     ['oper.familyName', 'debtorAcc.agent.lastName'],
     ['oper.numbers', 'debtorAcc.agent.nums'],
 ];
 
-function map(obj: any, fieldMap: [string, string][]) {
+function map(obj: any, fieldMap: FieldMap) {
     const deflatedSrc = deflate(obj);
     const deflatedTarget: { [x: string]: any; } = {};
     fieldMap.forEach( mapping => {
         deflatedTarget[mapping[1]] = deflatedSrc[mapping[0]];
     });
-    console.log(obj);
-    console.log(deflatedSrc);
-    console.log(deflatedTarget);
-    const mapped = inflate(deflatedTarget);
-    console.log(mapped);
+    return inflate(deflatedTarget);
+}
+
+class Trio {
+    constructor(
+        private person1: Person,
+        private person2: Person,
+        private person3: Person,
+      ) {}
+}
+
+class Person {
+    constructor(
+        private firstName: string | null,
+        private lastLast: string | null,
+        private homeAddr: Address,
+        private workAddr: Address,
+      ) {}
+}
+
+class Address {
+    constructor(
+        private line1: string | null,
+        private line2: string | null,
+        private city: string | null,
+      ) {}
+}
+
+const trio = new Trio(
+    new Person(null, null, new Address(null, null, null), new Address(null, null, null)),
+    new Person(null, null, new Address(null, null, null), new Address(null, null, null)),
+    new Person(null, null, new Address(null, null, null), new Address(null, null, null))
+);
+
+
+const data = {
+    debtors: {
+        debtorAgt1: {
+            givenName: 'John',
+            familyName: 'Smith',
+            postalAddr: {
+                ln1: '25 The Beeches',
+                cityName: 'New York'
+            }
+        },
+        debtorAgt2: {
+            givenName: 'Jane',
+            familyName: 'Warrick',
+            postalAddr: {
+                ln1: '1230 Pennybrook Lane',
+                cityName: 'London'
+            }
+
+        },
+        debtorAgt3: {
+            givenName: 'Lou',
+            familyName: 'Greene',
+            postalAddr: {
+                ln1: '102 Long Street',
+                cityName: 'Paris'
+            }
+        },
+    }
+};
+
+type FieldMap = [string, string][];
+
+const masterFieldMap = [
+    ['static', ''],
+    ['creditors', 'creds'],
+    ['debtors', 'debs'],
+] as FieldMap;
+
+const trioFieldMap = [
+    ['debtors.debtorAgt1', 'person1'],
+    ['debtors.debtorAgt2', 'person2'],
+    ['debtors.debtorAgt3', 'person3'],
+] as FieldMap;
+
+const addrFieldMap = [
+    ['l1', 'line1'],
+    ['l2', 'line2'],
+    ['cityName', 'city'],
+] as FieldMap;
+
+const personFieldMap = [
+    ['givenName', 'firstName'],
+    ['familyName', 'lastName'],
+]  as FieldMap;
+
+class Builder {
+
+    constructor(private masterFieldMap: FieldMap) {
+    }
+
+    meld_append(map1: FieldMap, map2: FieldMap): Builder {
+        const melded = [] as FieldMap;
+        map1.forEach(pair => {
+            map2.forEach(subPair => {
+                melded.push([`${pair[0]}.${subPair[0]}`, `${pair[1]}.${subPair[1]}`]);
+            });
+        });
+        this.masterFieldMap = this.masterFieldMap.concat(melded);
+        return this;
+    }
+
+    final() {
+        return this.masterFieldMap;
+    }
+}
+
+function doMap() {
+    const partyFieldMap = new Builder(personFieldMap.concat());
+    partyFieldMap.meld_append([
+        ['workAddr', 'postalAddr']], addrFieldMap);
+    
+    const builder = new Builder(masterFieldMap);
+    builder.meld_append([
+        ['creditors.agent', 'creds.agt'],
+        ['creditors.party1', 'creds.person1'],
+        ['creditors.party2', 'creds.person1'],
+        ['debtors.agent', 'creds.agt'],
+        ['debtors.party1', 'creds.person1'],
+        ['debtors.party2', 'creds.person2'],
+    ], partyFieldMap.final());
+
+    return builder.final();
 }
